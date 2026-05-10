43745da61284ff2bf808521fe3a7a7300a1bc0cc
TigetTiger <chris691228@gmail.com>
2026-05-10 11:09:10 +0800

    fix: add full PATH for cron environment, use nohup

--- reload-netsearch.sh
+++ reload-netsearch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+export PATH=/root/.nvm/versions/node/v20.20.2/bin:/usr/local/bin:/usr/bin:/bin:$PATH
+
+# 精準殺掉 3002
 kill $(lsof -ti :3002) 2>/dev/null
-sleep 2
+sleep 3
+
 cd /home/local/SSO/yt0115/NetSearch_sqlite
-npm run dev >> /tmp/netsearch.log 2>&1 &
+
+nohup npm run dev >> /tmp/netsearch.log 2>&1 &
+
+echo "[$(date)] restarted PID=$!" >> /tmp/netsearch-restart.log
